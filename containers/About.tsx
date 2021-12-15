1da60d783502b239a99d4e090fc67da98b25789f
João Pinheiro <joaotomasp@gmail.com>
2021-12-15 02:39:36 +0000

    Updated changes

--- containers/About.tsx
+++ containers/About.tsx
@@ -3,10 +3,8 @@ import Button from "@components/Button";
 import { Routes } from "@utils/enums";
 import styles from "@styles/About.module.scss";
 import { ArrowRight } from "phosphor-react";
-import React from "@images/react.svg";
-import Typescript from "@images/typescript.svg";
-import Sass from "@images/sass.svg";
-import Redux from "@images/redux.svg";
+import Flutter from "@images/flutter.svg";
+import Dart from "@images/dart.svg";
 
 export default function About() {
   return (
@@ -14,8 +12,8 @@ export default function About() {
       <header className={styles.header}>
         <h1 id={`${Routes.ABOUT}-header`}>About</h1>
         <p>
-          I&apos;m an ambitious frontend developer ready to build and design
-          digital experiences through the web.
+          I&apos;m an ambitious mobile developer ready to build and design
+          digital experiences through mobile technology.
         </p>
       </header>
       <div className={styles.row}>
@@ -23,10 +21,10 @@ export default function About() {
           <div className={styles.hard}>
             <h2>Hard skills</h2>
             <ul className={styles.list}>
-              <li>React.js</li>
+              <li>Flutter</li>
+              <li>Dart</li>
+              <li>React Native</li>
               <li>Typescript</li>
-              <li>Sass</li>
-              <li>Redux</li>
             </ul>
           </div>
           <div className={styles.soft}>
@@ -40,10 +38,8 @@ export default function About() {
           </div>
         </div>
         <div className={styles.logos}>
-          <Image src={React} alt="React.js" />
-          <Image src={Sass} alt="Sass" />
-          <Image src={Typescript} alt="Typescript" />
-          <Image src={Redux} alt="Redux" />
+          <Image src={Flutter} alt="Flutter" />
+          <Image src={Dart} alt="Dart" />
         </div>
       </div>
 
